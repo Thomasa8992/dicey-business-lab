19a9f64eb8f6fd332d8dd172017b7b40260164f1
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-07-09 15:24:39 -0500

    finished 2 extra credits

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 var random,
 dieArray = [], 
-genDie = document.querySelector('#Gen-die'),
+genDie = document.querySelector('#gen-die'),
 rollDice = document.querySelector('#roll-dice'),
 sum = document.querySelector('#sum');
+
 document.addEventListener('DOMContentLoaded', function(){
     var Die = function() {
         var randomNumber = Math.floor((Math.random()*6)+1);
@@ -12,31 +13,40 @@ document.addEventListener('DOMContentLoaded', function(){
         this.dieDiv.className = 'die-div';
         document.body.appendChild(this.dieDiv); 
         this.dieDiv.innerText = randomNumber;
-
-        
+        this.dieDiv.addEventListener('dblclick', this.removeDiv.bind(this));   
+        this.dieDiv.addEventListener('click', this.roll.bind(this));
+        sum.addEventListener('click', this.sum) 
     }
     Die.prototype.roll = function() {
-            this.value = Math.floor((Math.random()*6)+1); 
-            this.dieDiv.innerText = this.value;
+        this.value = Math.floor((Math.random()*6)+1); 
+        this.dieDiv.innerText = this.value;
     }  
+    Die.prototype.removeDiv = function() {
+        this.dieDiv.remove();
+        dieArray.pop();
+        console.log(dieArray);
+    }  
+
+
     genDie.addEventListener('click', function(){
         var die = new Die();
         dieArray.push(die);
         console.log(dieArray);
+
     });
     rollDice.addEventListener('click', function(){
         for(var i = 0; i < dieArray.length; i++){
             dieArray[i].roll();
+            dieArray.values()   
         }
      });
-    Die.prototype.add = function() {
-        this.value = Math.floor((Math.random()*6)+1); 
-        this.dieDiv.innerText = this.value;
-    }  
     sum.addEventListener('click', function(){
-        for(var i = 0; i < dieArray.length; i++){
-            dieArray[i].roll();
-        }
+        var myTotal = 0;  // Variable to hold your total
+
+        for(var i = 0; i < dieArray.length; i++) {
+            console.log( myTotal += dieArray[i]);
+}
+
     });
    
     
